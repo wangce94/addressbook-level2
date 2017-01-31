c511fb23109fe1d092578095d8bc6d466d45af28
WANGCE\WangC <wangce999@hotmail.com>
2017-02-01 00:48:25 +0800

    implemented PostalCode class

---
+++ src/seedu/addressbook/data/person/PostalCode.java
@@ -0,0 +1,13 @@
+package seedu.addressbook.data.person;
+
+public class PostalCode {
+    private String value;
+
+    public PostalCode(String value) {
+	this.value = value;
+    }
+
+    public String getValue() {
+        return value;
+    }
+}
