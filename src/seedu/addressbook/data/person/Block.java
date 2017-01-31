e16a338bc7af814c03d8b190e90d663aaa3fa9ef
WANGCE\WangC <wangce999@hotmail.com>
2017-02-01 00:45:29 +0800

    implemented block class

---
+++ src/seedu/addressbook/data/person/Block.java
@@ -0,0 +1,13 @@
+package seedu.addressbook.data.person;
+
+public class Block {
+    private String value;
+
+    public Block(String blockValue) {
+	this.value = blockValue;
+    }
+
+    public String getValue() {
+        return value;
+    }
+}
