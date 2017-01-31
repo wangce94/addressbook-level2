804e016995c9ac2f61f01cca528f62fb9de890b9
WANGCE\WangC <wangce999@hotmail.com>
2017-02-01 00:46:14 +0800

    implemented Street class

---
+++ src/seedu/addressbook/data/person/Street.java
@@ -0,0 +1,17 @@
+
+package seedu.addressbook.data.person;
+
+
+public class Street {
+
+    private String value;
+    
+    public Street(String streetValue) {
+	value = streetValue;
+    }
+    
+    public String getValue() {
+        return value;
+    }
+
+}
