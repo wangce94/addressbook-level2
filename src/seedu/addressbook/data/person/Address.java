aca10d92eb5ed3b0ab4f0ce21f7a40e3bdb8c251
WANGCE\WangC <wangce999@hotmail.com>
2017-02-09 21:26:37 +0800

    implemented getPrintableString in Address

--- src/seedu/addressbook/data/person/Address.java
+++ src/seedu/addressbook/data/person/Address.java
@@ -6,11 +6,12 @@ import seedu.addressbook.data.exception.IllegalValueException;
  * Represents a Person's address in the address book.
  * Guarantees: immutable; is valid as declared in {@link #isValidAddress(String)}
  */
-public class Address {
+public class Address implements Printable{
 
     public static final String EXAMPLE = "123, some street";
     public static final String MESSAGE_ADDRESS_CONSTRAINTS = "Person addresses can be in any format";
     public static final String ADDRESS_VALIDATION_REGEX = ".+";
+    public static final String ADDRESS_PREFIX = "Address: ";
 
     public final String value;
     private boolean isPrivate;
@@ -34,6 +35,10 @@ public class Address {
      */
     public static boolean isValidAddress(String test) {
         return test.matches(ADDRESS_VALIDATION_REGEX);
+    }
+    @Override
+    public String getPrintableString() {
+	return ADDRESS_PREFIX + this.value;
     }
 
     @Override
