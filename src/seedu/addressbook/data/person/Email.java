1d94c225f59f453de93c4679f1a3b671445054a7
WANGCE\WangC <wangce999@hotmail.com>
2017-02-09 21:27:55 +0800

    implemented getPrintableString in Email

--- src/seedu/addressbook/data/person/Email.java
+++ src/seedu/addressbook/data/person/Email.java
@@ -6,12 +6,13 @@ import seedu.addressbook.data.exception.IllegalValueException;
  * Represents a Person's email in the address book.
  * Guarantees: immutable; is valid as declared in {@link #isValidEmail(String)}
  */
-public class Email {
+public class Email implements Printable{
 
     public static final String EXAMPLE = "valid@e.mail";
     public static final String MESSAGE_EMAIL_CONSTRAINTS =
             "Person emails should be 2 alphanumeric/period strings separated by '@'";
     public static final String EMAIL_VALIDATION_REGEX = "[\\w\\.]+@[\\w\\.]+";
+    public static final String EMAIL_PREFIX = "Email: ";
 
     public final String value;
     private boolean isPrivate;
@@ -35,6 +36,10 @@ public class Email {
      */
     public static boolean isValidEmail(String test) {
         return test.matches(EMAIL_VALIDATION_REGEX);
+    }
+    @Override
+    public String getPrintableString() {
+	return EMAIL_PREFIX + this.value;
     }
 
     @Override
